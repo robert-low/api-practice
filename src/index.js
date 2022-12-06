6d07444f095df51e4b29d2b0b2616f06cda68ed6
Robert Low <robert.low91@gmail.com>
2022-12-06 16:41:56 +0000

    fetch movies

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ console.log("Hello from src/index.js!");
 // GET REQUEST - used to pull information from an API.
 // 1. Find the base URL of the API (token is added from .env file)
 
-const baseURL = `http://www.omdbapi.com/?s=firm&apikey=48727053`;
+const baseURL = `http://www.omdbapi.com/?s=matrix&apikey=48727053`;
+const results = document.querySelector("#results");
 
 fetch(`${baseURL}`)
   .then(response => response.json())
   .then((data) => {
-    console.log(data);
+    console.log(data.Search);
+    data.Search.forEach((result) => {
+      console.log(result);
+    });
   });
