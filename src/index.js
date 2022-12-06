f0e6799c22addaf4e7940a03fcc0fdd6ad620baa
Robert Low <robert.low91@gmail.com>
2022-12-06 16:20:22 +0000

    removed .env file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+
 console.log("Hello from src/index.js!");
 
-// GET REQUEST
+// GET REQUEST - used to pull information from an API.
+// 1. Find the base URL of the API (token is added from .env file)
+
+const baseURL = `http://www.omdbapi.com/?s=firm&apikey=48727053`;
+
+fetch(`${baseURL}`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
