cd610c98f2f8df95d9b72bb02fff5f8d8ee2c443
Robert Low <robert.low91@gmail.com>
2022-12-06 17:31:40 +0000

    dynamic html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-require('dotenv').config();
-
-console.log("Hello from src/index.js!");
-
 // GET REQUEST - used to pull information from an API.
-// 1. Find the base URL of the API (token is added from .env file)
 
 const baseURL = `http://www.omdbapi.com/?s=matrix&apikey=48727053`;
-const results = document.querySelector("#results");
+const unorderedMovieList = document.querySelector("#results");
 
 fetch(`${baseURL}`)
   .then(response => response.json())
   .then((data) => {
-    console.log(data.Search);
-    data.Search.forEach((result) => {
-      console.log(result);
+    data.Search.forEach((movieResult) => {
+      const movieListItem = `<li class="movie-title-poster">
+      <img src=${movieResult.Poster}>
+      <p>${movieResult.Title}</p>
+      </li>`;
+      unorderedMovieList.insertAdjacentHTML("beforeend", movieListItem);
     });
   });
+
+// 1. Define the Base URL of the API we want info from.
+// 2. Parse this information as a JSON file.
+// 3. data is the JSON object retrieved from the API.
+// 4. data.Search is the array of the movie results within the data object -->
+// { Search: [{movieResult}]}.
+// 6. movieResult is an one element (an object) of the array --> {movieResult}
+// 7. movieListItem is the HTML string with one object of the array and it's keys Title, Poster -->
+// 8. the variable above selects the HTML element in index.HTML by its ID - results.
+// 9. it then inserts into it the variable defined above into the DOM, which produces HTML.
